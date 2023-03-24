5aef20c9831c9920e45b6f6399b1ca1ce5d905d2
Love Verma <love.verma@trantorchd.com>
2023-03-24 18:25:09 +0530

    Projects & Work Experience responsive updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,28 @@ import { BannerComponent } from './banner/banner.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
 import {MatIconModule} from '@angular/material/icon';
 import {MatChipsModule} from '@angular/material/chips';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { ExprienceDetailComponent } from './work-experience/exprience-detail/exprience-detail.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectCardComponent } from './projects/project-card/project-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     WorkExperienceComponent,
-    ExprienceDetailComponent
+    ExprienceDetailComponent,
+    ProjectsComponent,
+    ProjectCardComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatChipsModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
